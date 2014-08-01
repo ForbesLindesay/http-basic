8c1fcbeb4acc928165b0311f101eeb36d6375c10
Forbes Lindesay <forbes.lindesay@red-gate.com>
2014-08-01 15:33:21 +0100

    Support cache and followRedirects

--- README.md
+++ README.md
@@ -15,12 +15,10 @@ Very low level wrapper arround http.request/https.request
 ```js
 var request = require('http-basic');
 
-var req = request('GET', 'http://example.com', options);
-req.on('response', function (res) {
+var req = request('GET', 'http://example.com', options, function (err, res) {
+  if (err) throw err;
   console.dir(res.statusCode);
-});
-req.on('error', function (err) {
-  throw err;
+  res.body.resume();
 });
 req.end();
 ```
@@ -37,10 +35,39 @@ The url as a string (e.g. `http://example.com`).  It must be fully qualified and
 
  - headers - http headers (defaults to `{}`)
  - agent (default: `false`)
+ - followRedirects (default: `false`) - if true, redirects are followed (note that this only affects the result in the callback)
+ - cache (default: `null`) - Either a Cache implementation (see below) or `'memory'` to use the default in-memory cache or `file` to use the default file-system cache.
+
+**callback:**
+
+The callback is called with `err` as the first argument and `res` as the second argument. `res` is an [http-response-object](https://github.com/ForbesLindesay/http-response-object).  It has the following properties:
+
+ - `statusCode` a number representing the HTTP Status Code
+ - `headers` an object representing the HTTP headers
+ - `body` a stream respresenting the request body.
 
 **returns:**
 
-[http.ClientRequest](http://nodejs.org/api/http.html#http_class_http_clientrequest)
+[http.ClientRequest](http://nodejs.org/api/http.html#http_class_http_clientrequest) if the method is duplex (i.e. `!(method === 'GET' || method === 'DELETE' || method === 'HEAD')`), otherwise `undefined`.
+
+### Implementing a Cache
+
+A `Cache` is an object with two methods:
+
+ - `getResponse(url, callback)` - retrieve a cached response object
+ - `setResponse(url, response)` - cache a response object
+
+A cached response object is an object with the following properties:
+
+ - `statusCode` - Number
+ - `headers` - Object (key value pairs of strings)
+ - `body` - Stream (a stream of binary data)
+ - `requestHeaders` - Object (key value pairs of strings)
+ - `requestTimestamp` - Number
+
+`getResponse` should call the callback with an optional error and either `null` or a cached response object, depending on whether the url can be found in the cache.  Only `GET`s are cached.
+
+`setResponse` should just swallow any errors it has (or resport them using `console.warn`).
 
 ## License
 
--- index.js
+++ index.js
@@ -2,14 +2,25 @@
 
 var parseUrl = require('url').parse;
 var protocols  = {http: require('http'), https: require('https')};
+var PassThrough = require('stream').PassThrough;
+var Response = require('http-response-object');
+var cacheUtils = require('./lib/cache-utils.js');
+var builtinCaches = {
+  memory: new (require('./lib/memory-cache.js'))(),
+  file: new (require('./lib/file-cache.js'))(__dirname + '/cache')
+};
 
 module.exports = request;
-function request(method, url, options) {
+function request(method, url, options, callback) {
   if (typeof method !== 'string') {
     throw new TypeError('The method must be a string.');
   }
   if (typeof url !== 'string') {
     throw new TypeError('The URL/path must be a string.');
+  }
+  if (typeof options === 'function') {
+    callback = options;
+    options = null;
   }
   if (options === null || options === undefined) {
     options = {};
@@ -18,17 +29,98 @@ function request(method, url, options) {
     throw new TypeError('options must be an object (or null)');
   }
 
-  url = parseUrl(url);
+  method = method.toUpperCase();
+  var urlString = url;
+  url = parseUrl(urlString);
 
-  var headers = options.headers || {};
+  var headers = {};
+  Object.keys(options.headers || {}).forEach(function (header) {
+    headers[header.toLowerCase()] = options.headers[header];
+  });
   var agent = 'agent' in options ? options.agent : false;
 
-  return protocols[url.protocol.replace(/\:$/, '')].request({
-    host: url.host,
-    port: url.port,
-    path: url.path,
-    method: method.toUpperCase(),
-    headers: headers,
-    agent: agent
-  });
+  var cache = options.cache;
+  if (typeof cache === 'string' && cache in builtinCaches) {
+    cache = builtinCaches[cache];
+  }
+  if (cache && !(typeof cache === 'object' && typeof cache.getResponse === 'function' && typeof cache.setResponse === 'function')) {
+    throw new TypeError(cache + ' is not a valid cache, caches must have `getResponse` and `setResponse` methods.');
+  }
+
+  var responded = false;
+
+  var duplex = !(method === 'GET' || method === 'DELETE' || method === 'HEAD');
+
+  function doRequest(callback) {
+    return protocols[url.protocol.replace(/\:$/, '')].request({
+      host: url.host,
+      port: url.port,
+      path: url.path,
+      method: method,
+      headers: headers,
+      agent: agent
+    }, function (res) {
+      if (responded) return res.resume();
+      responded = true;
+      if (options.followRedirects && isRedirect(res.statusCode)) {
+        return request(duplex ? 'GET' : method, res.headers.location, options, callback);
+      }
+      callback(null, new Response(res.statusCode, res.headers, res));
+    }).on('error', function (err) {
+      if (responded) return;
+      responded = true;
+      callback(err);
+    });
+  }
+
+  if (duplex) {
+    return doRequest(callback);
+  } else {
+    if (cache && method === 'GET') {
+      var timestamp = Date.now();
+      cache.getResponse(urlString, function (err, cachedResponse) {
+        if (err) {
+          console.warn('Error reading from cache: ' + err.message);
+        }
+        if (cachedResponse && cacheUtils.isMatch(headers, cachedResponse)) {
+          if (!cacheUtils.isExpired(cachedResponse)) {
+            var res = new Response(cachedResponse.statusCode, cachedResponse.headers, cachedResponse.body);
+            res.fromCache = true;
+            res.fromNotModified = false;
+            return callback(null, res);
+          } else if (cachedResponse.headers['etag']) {
+            headers['if-none-match'] = cachedResponse.headers['etag'];
+          }
+        }
+        doRequest(function (err, res) {
+          if (err) return callback(err);
+          if (res.statusCode === 304 && cachedResponse) { // Not Modified
+            res = new Response(cachedResponse.statusCode, cachedResponse.headers, cachedResponse.body);
+            res.fromCache = true;
+            res.fromNotModified = true;
+            return callback(null, res);
+          } else if (cacheUtils.canCache(res)) {
+            var cachedResponseBody = new PassThrough();
+            var resultResponseBody = new PassThrough();
+            res.body.on('data', function (data) { cachedResponseBody.write(data); resultResponseBody.write(data); });
+            res.body.on('end', function () { cachedResponseBody.end(); resultResponseBody.end(); });
+            var responseToCache = new Response(res.statusCode, res.headers, cachedResponseBody);
+            var resultResponse = new Response(res.statusCode, res.headers, resultResponseBody);
+            responseToCache.requestHeaders = headers;
+            responseToCache.requestTimestamp = timestamp;
+            cache.setResponse(urlString, responseToCache);
+            return callback(null, resultResponse);
+          } else {
+            return callback(null, res);
+          }
+        }).end();
+      });
+    } else {
+      doRequest(callback).end();
+    }
+  }
+}
+
+function isRedirect(statusCode) {
+  return statusCode === 301 || statusCode === 302 || statusCode === 307 || statusCode === 308;
 }
