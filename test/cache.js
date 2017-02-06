9df385c7b887563c4866d009dfa15e35f5ad3ad2
Andrew Stewart Gibson <andrew@goofballLogic.com>
2017-02-06 14:40:03 +0000

    Allow more valid formats for cache-control header (#15)
    * Allows a wider range of valid cache-control header values
    
    * Added negative test for cache-control header (private)

--- test/cache.js
+++ test/cache.js
@@ -6,18 +6,38 @@ var http = require('http');
 var serveStatic = require('serve-static');
 var rimraf = require('rimraf');
 var path = require('path');
+var url = require('url');
+var qs = require('querystring');
 
 rimraf.sync(path.resolve(__dirname, '..', 'cache'));
 
 
 var CACHED_BY_CACHE_CONTROL = 'http://localhost:3293/index.js';
+var CACHED_BY_CACHE_CONTROL__MAX_AGE = CACHED_BY_CACHE_CONTROL +'?cache-control='+encodeURIComponent('max-age=60');
+var CACHED_BY_CACHE_CONTROL__MAX_AGE_PUBLIC = CACHED_BY_CACHE_CONTROL +'?cache-control='+encodeURIComponent('max-age=60,public');
+var CACHED_BY_CACHE_CONTROL__MAX_AGE_PRIVATE = CACHED_BY_CACHE_CONTROL +'?cache-control='+encodeURIComponent('max-age=60,private');
+var CACHED_BY_CACHE_CONTROL__LWS = CACHED_BY_CACHE_CONTROL +'?cache-control='+encodeURIComponent('  public  ,  max-age=60  ');
+
+
+var overrideableHeaders = ['cache-control'];
+function overrideHeaders(res, path, stat) {
+
+  var query = qs.parse(url.parse(this.req.url).query);
+  overrideableHeaders.forEach(function(header){
+    if(header in query) {
+      res.setHeader(header,query[header]);
+    }
+  });
+  
+}
 
 var cacheControlServer = http.createServer(serveStatic(__dirname, {
   etag: false,
   lastModified: false,
   cacheControl: true,
   maxAge: 5000,
-  fallthrough: false
+  fallthrough: false,
+  setHeaders: overrideHeaders
 }));
 
 cacheControlServer.listen(3293, function onListen() {
@@ -64,6 +84,97 @@ cacheControlServer.listen(3293, function onListen() {
           res.body.resume();
         });
       }, 1000);
+    });
+  });
+
+  request('GET', CACHED_BY_CACHE_CONTROL__MAX_AGE, {cache: 'memory'}, function(err, res){
+     if (err) throw err;
+
+    console.log('response E.1 ("max-age=60")');
+    assert(res.statusCode === 200);
+    assert(res.fromCache === undefined);
+    assert(res.fromNotModified === undefined);
+    res.body.on('data', function () {});
+    res.body.on('end', function () {
+      setTimeout(function () {
+        request('GET', CACHED_BY_CACHE_CONTROL__MAX_AGE, {cache: 'memory'}, function (err, res) {
+          if (err) throw err;
+
+          console.log('response F.1 ("max-age=60")');
+          assert(res.statusCode === 200);
+          assert(res.fromCache === true);
+          assert(res.fromNotModified === false);
+          res.body.resume();
+        });
+      }, 25);
+    });
+  });
+  
+  request('GET', CACHED_BY_CACHE_CONTROL__MAX_AGE_PUBLIC, {cache: 'memory'}, function(err, res){
+     if (err) throw err;
+
+    console.log('response E.2 ("max-age=60,public)');
+    assert(res.statusCode === 200);
+    assert(res.fromCache === undefined);
+    assert(res.fromNotModified === undefined);
+    res.body.on('data', function () {});
+    res.body.on('end', function () {
+      setTimeout(function () {
+        request('GET', CACHED_BY_CACHE_CONTROL__MAX_AGE_PUBLIC, {cache: 'memory'}, function (err, res) {
+          if (err) throw err;
+
+          console.log('response F.2 ("max-age=60,public)');
+          assert(res.statusCode === 200);
+          assert(res.fromCache === true);
+          assert(res.fromNotModified === false);
+          res.body.resume();
+        });
+      }, 25);
+    });
+  });
+  
+  request('GET', CACHED_BY_CACHE_CONTROL__MAX_AGE_PRIVATE, {cache: 'memory'}, function(err, res){
+     if (err) throw err;
+
+    console.log('response E.3 ("max-age=60,private)');
+    assert(res.statusCode === 200);
+    assert(res.fromCache === undefined);
+    assert(res.fromNotModified === undefined);
+    res.body.on('data', function () {});
+    res.body.on('end', function () {
+      setTimeout(function () {
+        request('GET', CACHED_BY_CACHE_CONTROL__MAX_AGE_PRIVATE, {cache: 'memory'}, function (err, res) {
+          if (err) throw err;
+
+          console.log('response F.3 ("max-age=60,private)');
+          assert(res.statusCode === 200);
+          assert(!res.fromCache);
+          res.body.resume();
+        });
+      }, 25);
+    });
+  });
+  
+  request('GET', CACHED_BY_CACHE_CONTROL__LWS, {cache: 'memory'}, function(err, res){
+     if (err) throw err;
+
+    console.log('response E.4 ("  public  ,  max-age=60  ")');
+    assert(res.statusCode === 200);
+    assert(res.fromCache === undefined);
+    assert(res.fromNotModified === undefined);
+    res.body.on('data', function () {});
+    res.body.on('end', function () {
+      setTimeout(function () {
+        request('GET', CACHED_BY_CACHE_CONTROL__MAX_AGE_PUBLIC, {cache: 'memory'}, function (err, res) {
+          if (err) throw err;
+
+          console.log('response F.4 ("  public  ,  max-age=60  ")');
+          assert(res.statusCode === 200);
+          assert(res.fromCache === true);
+          assert(res.fromNotModified === false);
+          res.body.resume();
+        });
+      }, 25);
     });
   });
 });
