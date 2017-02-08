235c8a4aacef1a3788025a9ed4a94587fd6a4240
Forbes Lindesay <forbes@lindesay.co.uk>
2017-02-08 17:11:08 +0000

    Fix inheritance in test fixture

--- test-fixture/invalidation-failure-cache.js
+++ test-fixture/invalidation-failure-cache.js
@@ -1,12 +1,16 @@
 'use strict';
 
 var MemoryCache = require('../lib/memory-cache');
+
 module.exports = InvalidationFailCache;
+
 function InvalidationFailCache(){
-    MemoryCache.call(this);
-    InvalidationFailCache.constructor = InvalidationFailCache;
+  MemoryCache.call(this);
+  InvalidationFailCache.constructor = InvalidationFailCache;
 }
-InvalidationFailCache.prototype = new MemoryCache();
+InvalidationFailCache.prototype = Object.create(MemroyCache.prototype);
+InvalidationFailCache.prototype.constructor = InvalidationFailCache;
+
 InvalidationFailCache.prototype.invalidateResponse = function (url, callback) {
-    callback(new Error('Invalidation failed'));
+  callback(new Error('Invalidation failed'));
 };
